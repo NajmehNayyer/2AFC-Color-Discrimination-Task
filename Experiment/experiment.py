1877df38eeb48876aee17841d40705c5c5d37d78
Najmeh Nayyer <najmehnayyer@gmail.com>
2026-07-09 11:00:22 +0330

    Move colors to colors.py

--- Experiment/experiment.py
+++ Experiment/experiment.py
@@ -2,22 +2,7 @@ from psychopy import visual, core, event, data, gui
 import numpy as np
 from constants import *
 from functions import *
-
-# ====================================
-# Experiment Colors
-# ====================================
-
-# RGB values for the experiment colors
-blue_steps = np.arange(0, 250, 25)
-yellow_steps = list(reversed(blue_steps))
-
-# RGB of each spectrum
-blue = [rgb_to_psy([b, b, 255]) for b in blue_steps]
-yellow = [rgb_to_psy([255, 255, y]) for y in yellow_steps]
-
-# Finalize the colors for the experiment
-color_data = [('yellow', rgb) for rgb in yellow] + [('blue', rgb) for rgb in blue]
-colors = [{'index': i, 'rgb': rgb, 'name': name} for i, (name, rgb) in enumerate(color_data)]
+from colors import colors
 
 # ====================================
 # Set-up
